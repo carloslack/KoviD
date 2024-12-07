c63282c6d665cb34ab00603a1d0004e00261c243
JNE <jne@non-negative.org>
2024-12-07 23:52:27 +0000

    fs: Make all hidden names harder to notice
    Example for a hidden directory "test"
    
    $ ls test
    ls: cannot access 'test': No such file or directory
    $ ls /home/Codes/test
    ls: cannot access '/home/Codes/test': No such file or directory
    $
    
    Previously, only PROCNAME enjoyed this "privilege"

--- src/fs.c
+++ src/fs.c
@@ -178,6 +178,22 @@ bool fs_search_name(const char *name, u64 ino) {
     return false;
 }
 
+const char *fs_get_basename(const char *path) {
+    char *base = NULL;
+
+    if (path == NULL || *path == '\0')
+        return path;
+
+    base = strrchr(path, '/');
+    if (!base)
+        return path;
+
+    if (base == path)
+        return base + 1;
+
+    return base + 1;
+}
+
 int fs_is_dir_inode_hidden(const char *name, u64 ino) {
     struct hidden_names *node, *node_safe;
     int count = 0;
