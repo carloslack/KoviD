4a8c7123035ed3468b59ef547f77de51fa0d6907
JNE <jne@non-negative.org>
2024-12-30 19:00:33 +0000

    tty: Try to fix issues after a difficult merge

--- src/fs.c
+++ src/fs.c
@@ -212,34 +212,6 @@ const char *fs_get_basename(const char *path)
 	return base + 1;
 }
 
-int fs_is_dir_inode_hidden(const char *name, u64 ino)
-{
-	struct hidden_names *node, *node_safe;
-	int count = 0;
-	list_for_each_entry_safe(node, node_safe, &names_node, list) {
-		if (node->is_dir && ino == node->ino_parent)
-			count++;
-	}
-	return count;
-}
-
-const char *fs_get_basename(const char *path)
-{
-	char *base = NULL;
-
-	if (path == NULL || *path == '\0')
-		return path;
-
-	base = strrchr(path, '/');
-	if (!base)
-		return path;
-
-	if (base == path)
-		return base + 1;
-
-	return base + 1;
-}
-
 void fs_list_names(void)
 {
 	struct hidden_names *node, *node_safe;
