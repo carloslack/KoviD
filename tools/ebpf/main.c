b6753cdb38ee34c745c06de2b26de64bdd50cef8
dj <djolertrk@gmail.com>
2025-01-20 13:56:06 +0100

    ebpf: Fix JSON
    It will always need a closing ] at the end!

--- tools/ebpf/main.c
+++ tools/ebpf/main.c
@@ -1,14 +1,15 @@
 // This file is a user-space loader that attaches an eBPF socket_filter_prog
-// to a raw AF_PACKET socket. It periodically writes JSON objects containing
-// SSH (22) & HTTPS (443) packet counts, plus an HTTP snippet from port 8080,
-// to /tmp/ebpf_kovid.json.
+// to a raw AF_PACKET socket. It periodically appends JSON "snapshot" objects
+// into a single array in /tmp/ebpf_kovid.json. The JSON is valid from start to
+// finish if the program is killed normally (SIGINT, SIGTERM), because we close
+// the array.
 //
 // Usage example:
-//   1) Place socket_filter_bpf.o in /usr/bin/
-//   2) Launch a local server:  python3 -m http.server 8080 --bind 127.0.0.1
-//   3) Generate traffic:       wget http://127.0.0.1:8080/
-//   4) Run as root:            sudo /usr/bin/ebpf-kovid
-//   5) Tail /tmp/ebpf_kovid.json to see new JSON objects.
+//   1) Place socket_filter_bpf.o in /usr/bin/socket_filter_bpf.o
+//   2) Launch: python3 -m http.server 8080 --bind 127.0.0.1
+//   3) wget http://127.0.0.1:8080/
+//   4) sudo ./ebpf-kovid
+//   5) tail /tmp/ebpf_kovid.json to see the array updating.
 
 #include <arpa/inet.h>
 #include <bpf/bpf.h>
@@ -17,16 +18,17 @@
 #include <linux/if_packet.h>
 #include <net/ethernet.h>
 #include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-static const char *bpf_object_file      = "/usr/bin/socket_filter_bpf.o";
-static const char *bpf_prog_section     = "socket";
-static const char *bpf_map_name         = "port_count_map";
-static const char *http_snippet_map_name= "http_snippet_map";
+static const char *bpf_object_file       = "/usr/bin/socket_filter_bpf.o";
+static const char *bpf_prog_section      = "socket";
+static const char *bpf_map_name          = "port_count_map";
+static const char *http_snippet_map_name = "http_snippet_map";
 
 #define HTTP_MAX_BYTES 64
 
@@ -35,15 +37,40 @@ struct http_snippet {
     unsigned int used;
 };
 
+static int prog_fd = -1, map_fd = -1, snippet_map_fd = -1;
+static int sock_fd = -1;
+static struct bpf_object *obj = NULL;
+static FILE *fp = NULL;    // File pointer to /tmp/ebpf_kovid.json
+static int snapshot_count = 0; // Keep track of how many snapshots we've written
+
+/* We'll install a signal handler so we can close the JSON array gracefully. */
+static void cleanup_and_exit(int sig)
+{
+    if (fp) {
+        // Close the JSON array
+        fprintf(fp, "\n]\n");
+        fclose(fp);
+        fp = NULL;
+    }
+
+    if (sock_fd >= 0) close(sock_fd);
+    if (obj) bpf_object__close(obj);
+
+    fprintf(stderr, "\nCaught signal %d. Exiting.\n", sig);
+    exit(0);
+}
+
 int main(int argc, char **argv)
 {
-    struct bpf_object *obj = NULL;
     struct bpf_program *prog = NULL;
     struct bpf_map *map = NULL, *snippet_map = NULL;
-    int prog_fd = -1, map_fd = -1, snippet_map_fd = -1;
-    int sock_fd = -1, err;
+    int err;
 
-    // Open/load the BPF object
+    // 1) Setup signal handlers to clean up
+    signal(SIGINT, cleanup_and_exit);
+    signal(SIGTERM, cleanup_and_exit);
+
+    // 2) Open and load the BPF object
     obj = bpf_object__open_file(bpf_object_file, NULL);
     if (!obj) {
         fprintf(stderr, "ERROR: bpf_object__open_file(%s) failed\n",
@@ -54,11 +81,10 @@ int main(int argc, char **argv)
     err = bpf_object__load(obj);
     if (err) {
         fprintf(stderr, "ERROR: bpf_object__load() failed\n");
-        bpf_object__close(obj);
-        return 1;
+        goto cleanup;
     }
 
-    // Find the eBPF program by section
+    // 3) Find the eBPF program
     prog = bpf_object__find_program_by_title(obj, bpf_prog_section);
     if (!prog) {
         fprintf(stderr, "ERROR: couldn't find program section '%s'\n",
@@ -71,7 +97,7 @@ int main(int argc, char **argv)
         goto cleanup;
     }
 
-    // Find the "port_count_map"
+    // 4) Find the "port_count_map"
     map = bpf_object__find_map_by_name(obj, bpf_map_name);
     if (!map) {
         fprintf(stderr, "ERROR: couldn't find map '%s'\n", bpf_map_name);
@@ -83,67 +109,79 @@ int main(int argc, char **argv)
         goto cleanup;
     }
 
-    // Find "http_snippet_map"
+    // 5) Find "http_snippet_map"
     snippet_map = bpf_object__find_map_by_name(obj, http_snippet_map_name);
     if (!snippet_map) {
-        fprintf(stderr, "ERROR: couldn't find map '%s'\n", http_snippet_map_name);
+        fprintf(stderr, "ERROR: couldn't find map '%s'\n",
+                http_snippet_map_name);
         goto cleanup;
     }
     snippet_map_fd = bpf_map__fd(snippet_map);
     if (snippet_map_fd < 0) {
-        fprintf(stderr, "ERROR: bpf_map__fd(%s) failed\n", http_snippet_map_name);
+        fprintf(stderr, "ERROR: bpf_map__fd(%s) failed\n",
+                http_snippet_map_name);
         goto cleanup;
     }
 
-    // Create a raw packet socket and attach eBPF
+    // 6) Create a raw packet socket and attach eBPF
     sock_fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (sock_fd < 0) {
         perror("socket(AF_PACKET, SOCK_RAW, ETH_P_ALL)");
         goto cleanup;
     }
-    err = setsockopt(sock_fd, SOL_SOCKET, SO_ATTACH_BPF, &prog_fd, sizeof(prog_fd));
+    err = setsockopt(sock_fd, SOL_SOCKET, SO_ATTACH_BPF,
+                     &prog_fd, sizeof(prog_fd));
     if (err) {
         perror("setsockopt(SO_ATTACH_BPF)");
         goto cleanup;
     }
 
-    // Open /tmp/ebpf_kovid.json for appending
-    FILE *fp = fopen("/tmp/ebpf_kovid.json", "a");
+    // 7) Open /tmp/ebpf_kovid.json. Let's TRUNCATE it at start for fresh data.
+    fp = fopen("/tmp/ebpf_kovid.json", "w");
     if (!fp) {
         fprintf(stderr, "ERROR: Could not open /tmp/ebpf_kovid.json for writing.\n");
         goto cleanup;
     }
 
-    fprintf(fp, "// eBPF KoviD JSON Log\n");
+    // Start a single JSON array
+    fprintf(fp, "[\n");
     fflush(fp);
 
-    // 6) Periodically read counters & snippet, log to JSON
-    __u16 ports[2] = {22, 443};
+    // 8) Periodically read counters & snippet, log JSON
+    __u16 ports[2] = { 22, 443 };
 
     while (1) {
-        // Start a new JSON object
+        // If not the first snapshot, add a comma to separate objects
+        if (snapshot_count > 0) {
+            fprintf(fp, ",\n");
+        }
+
+        // Begin an object
         fprintf(fp, "{\n  \"snapshot\": [\n");
 
-        // (a) Port counters
+        // (a) port counters
         for (int i = 0; i < 2; i++) {
             __u64 value = 0;
             if (bpf_map_lookup_elem(map_fd, &ports[i], &value) == 0) {
                 // Print a JSON array element
-                fprintf(fp, "    { \"port\": %u, \"packets\": %llu }%s\n",
+                fprintf(fp,
+                        "    { \"port\": %u, \"packets\": %llu }%s\n",
                         ports[i], value, (i == 0 ? "," : ""));
             } else {
-                fprintf(fp, "    { \"port\": %u, \"packets\": 0 }%s\n",
+                fprintf(fp,
+                        "    { \"port\": %u, \"packets\": 0 }%s\n",
                         ports[i], (i == 0 ? "," : ""));
             }
         }
 
-        // (b) Optional HTTP snippet
+        fprintf(fp, "  ],\n"); // close the snapshot array
+
+        // (b) optional HTTP snippet
         {
             __u32 key = 0;
             struct http_snippet snippet;
             memset(&snippet, 0, sizeof(snippet));
 
-            fprintf(fp, "  ],\n"); // close the snapshot array
             if (bpf_map_lookup_elem(snippet_map_fd, &key, &snippet) == 0) {
                 if (snippet.used == 1) {
                     fprintf(fp, "  \"http_snippet\": \"");
@@ -157,29 +195,33 @@ int main(int argc, char **argv)
                         }
                     }
                     fprintf(fp, "\",\n");
-
                     // Mark snippet used=0 so we don't re-print
                     snippet.used = 0;
-                    bpf_map_update_elem(snippet_map_fd, &key, &snippet, BPF_ANY);
+                    bpf_map_update_elem(snippet_map_fd, &key, &snippet,
+                                        BPF_ANY);
                 } else {
                     // No snippet
                     fprintf(fp, "  \"http_snippet\": null,\n");
                 }
             } else {
-                // If the snippet lookup fails, also set null
                 fprintf(fp, "  \"http_snippet\": null,\n");
             }
         }
 
-        // finalize JSON object
-        fprintf(fp, "  \"note\": \"another snapshot\"\n}\n\n");
+        fprintf(fp, "  \"note\": \"another snapshot\"\n}\n"); // end object
         fflush(fp);
 
-        // Sleep 5 seconds
+        snapshot_count++;
         sleep(5);
     }
 
 cleanup:
+    if (fp) {
+        // If we exit the loop or error out, close the array properly
+        fprintf(fp, "\n]\n");
+        fclose(fp);
+        fp = NULL;
+    }
     if (sock_fd >= 0) close(sock_fd);
     if (obj) bpf_object__close(obj);
     return 0;
